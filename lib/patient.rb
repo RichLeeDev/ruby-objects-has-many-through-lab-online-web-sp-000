5a515cc20a3c0b228ae49d83a187ae09c13b7005
Richard Lee <richlee81192@gmail.com>
2019-11-11 02:53:16 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,30 @@
+class Patient
+  attr_accessor :name
+  
+  @@all = []
+  
+  def self.all
+    @@all 
+  end
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def new_appointment(date, doctor)
+    appointment = Appointment(date, self, doctor)
+  end
+  
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+  
+  def doctors
+    self.doctor.all.collect {|doctor| doctor.patient == self}
+  end
+end 
